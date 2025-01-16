f61f8063821051f0887aed3a5e10a0f8027bdcc3
example <example@example.com>
2025-01-16 17:04:48 +0900

    setting

--- Codes/generate_static_html.jl
+++ Codes/generate_static_html.jl
@@ -4,36 +4,84 @@ function generate_ranking_table()
         df = get_current_ranking()
         
         rows = String[]
+        push!(rows, """
+            <tr>
+                <th>順位</th>
+                <th>プレイヤー</th>
+                <th>現在レート</th>
+                <th class="buttonMax" id="Max_btn">最高レート</th>
+                <th>対戦成績</th>
+            </tr>
+        """)
         
         for i in 1:N
-            row = (img_url=df.url[i], name=df.Name[i], current_rate=df.Now[i], max_rate=df.Max[i], Log = df.Log[i], ID = df.ID[i])
+            row = (img_url=df.url[i], name=df.Name[i], current_rate=df.Now[i], max_rate=df.Max[i], Log = df.Log[i])
             
-            if row.max_rate == 1000
-                break
-            end
-
             player_html = """
-                        <div class="flex-box1">
-                            <div class="rank"></div>
-                            <div class="rank_num">$i</div>
-                            <div class="player-photo">
-                                <img src="$(row.img_url)" class="player-photo" alt="$(row.name)">
-                            </div>
-                            <div class="name">$(row.name)</div>
-                            <div class="fighter0"></div>
-                            <div class="fighter"><img src = "https://raw.githubusercontent.com/Aru-OU/Aru-OU.github.io/refs/heads/main/Figs/$(row.ID).avif" ></div>
-                            <div class="rate0">RATE</div>
-                            <div class="rate">$(row.current_rate) / $(row.max_rate)</div>
+                <tr>
+                    <td>$(i)</td>
+                    <td>
+                        <div class="player-info">
+                            <img src="$(row.img_url)" class="player-photo" alt="$(row.name)">
+                            $(row.name)
                         </div>
+                    </td>
+                    <td>
+                        $(round(Int, row.current_rate))
+                    </td>
+                    <td>
+                        $(round(Int, row.max_rate))
+                    </td>
+                    <td>$(row.Log)</td>
+                </tr>
             """
             push!(rows, player_html)
         end
         
-        table = HTML_TEMPLATE * join(rows) * """
-                </div>
-            </body>
-        </html>"""
-        return table
+        table1 = "<table class=\"Now_screen\">" * join(rows) * "</table>"
+        
+        rank2 = [df.Max[i]*10000 + df.Now[i] + 1/df.ID[i] for i in 1:N]
+        junban2 = sortperm(rank2, rev=true)
+        df2 = (ID=df.ID[junban2], Name = df.Name[junban2], url = df.url[junban2], Now = df.Now[junban2], Max = df.Max[junban2], Log = df.Log[junban2])
+        
+        rows2 = String[]
+        push!(rows2, """
+            <tr>
+                <th>順位</th>
+                <th>プレイヤー</th>
+                <th class="buttonNow" id="Now_btn">現在レート</th>
+                <th>最高レート</th>
+                <th>対戦成績</th>
+            </tr>
+        """)
+        
+        for i in 1:N
+            row = (img_url=df2.url[i], name=df2.Name[i], current_rate=df2.Now[i], max_rate=df2.Max[i], Log = df2.Log[i])
+            
+            player_html = """
+                <tr>
+                    <td>$(i)</td>
+                    <td>
+                        <div class="player-info">
+                            <img src="$(row.img_url)" class="player-photo" alt="$(row.name)">
+                            $(row.name)
+                        </div>
+                    </td>
+                    <td>
+                        $(round(Int, row.current_rate))
+                    </td>
+                    <td>
+                        $(round(Int, row.max_rate))
+                    </td>
+                    <td>$(row.Log)</td>
+                </tr>
+            """
+            push!(rows2, player_html)
+        end
+        
+        table2 = "<table class=\"Max_screen no\">" * join(rows2) * "</table>"
+        
+        return table1 * table2
     catch e
         @error "Error generating ranking table" exception=(e, catch_backtrace())
         return "<p>ランキングの読み込み中にエラーが発生しました。</p>"
@@ -43,9 +91,11 @@ end
 # HTMLファイルを生成して保存
 function generate_and_save_html()
     try
-        html = generate_ranking_table()
-        current_time = Dates.format(now(), "yyyy_mm_dd_HH:MM")
-        html = replace(html, "{{TIMESTAMP}}" => "$(current_time)")
+        ranking_table = generate_ranking_table()
+        current_time = Dates.format(now(), "mm/dd HH:MM")
+        html = replace(HTML_TEMPLATE, "{{RANKING_TABLE}}" => ranking_table)
+        html = replace(html, "{{TIMESTAMP}}" => "作成日時: $(current_time)")
+        
         # HTMLファイルを保存
         output_path = "./index.html"
         open(output_path, "w") do io
